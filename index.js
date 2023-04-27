90a31e3d6560407286105f18b34cff8f15e8cf8e
Aadit <baggaaadit017@gmail.com>
2023-04-27 21:59:30 +0530

    working

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-
+//
 client.login('MTEwMTE3MDU5NzMwOTk4NDgxOA.GDybrl.MUKIRA4prGNzJB7s6E70oFaQzKTvk6R9f2Tjlg')
