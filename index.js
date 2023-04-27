df6a49c9ac55228f5f73d1316937c5a22d86eedf
Aadit <baggaaadit017@gmail.com>
2023-04-27 21:58:37 +0530

    its working now

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login('MTEwMTE3MDU5NzMwOTk4NDgxOA.GDybrl.MUKIRA4prGNzJB7s6E70oFaQzKTvk6R9f2Tjlg')
