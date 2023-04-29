c21c69c2d1d0e906431415512bdb73876f0b1b55
Aadit <baggaaadit017@gmail.com>
2023-04-29 11:59:12 +0530

    pfp changed

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const client = new Client({intents: [
 	GatewayIntentBits.GuildMembers,
 ], });
 const ytdl = require('ytdl-core');
-const prefix="JITESH"
+const { jitesh } = require('./jitesh');
+const prefix="jitesh "
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -19,7 +20,7 @@ client.on("ready", () => {
 
 client.on("messageCreate", msg => {
 	if(!msg.author.bot){
-	if(msg.content.toUpperCase().startsWith(prefix)){
+	if(msg.content.toLowerCase().startsWith(prefix)){
 		if(basicCon(msg)!="0"){
 			// msg.channel.send(basicCon(msg))
 			// console.log(basicCon(msg)+"index.js");
@@ -29,46 +30,20 @@ client.on("messageCreate", msg => {
 	}
 }
 })
-var cats = require('cat-ascii-faces')
-// client.on("messageCreate", msg => {
-// 	if(msg.mentions.has(client.user)){
-// 		if(!msg.author.bot){
-// 			msg.reply(`${cats()}`)
-// 	}}})
+// var cats = require('cat-ascii-faces')
+const emojis=["💩","💀","👾","🧜","👙"]
+client.on("messageCreate", msg => {
+	if(msg.mentions.has(client.user) || msg.content.toLowerCase().includes("jitesh")){
+		if(!msg.author.bot){
+			msg.react(`${emojis[Math.floor(Math.random() * emojis.length)]}`)
+	}}})
 
-//https://www.freecodecamp.org/news/how-to-create-a-music-bot-using-discord-js-4436f5f3f0f8/
-const serverQueue=queue.get(message.guild.id)
-client.on("message", (msg) => {
+client.on("messageCreate", (msg) => {
 	if(!msg.author.bot){
-		if(msg.content.toLowerCase().startsWith(prefix+"play")){
-			execute(message,serverQueue);
-			return
-		}else if(msg.content.toLowerCase().startsWith(prefix+"skip")){
-				StickerPack(message,serverQueue)
-		}else if(msg.content.toLowerCase().startsWith(prefix+"stop")){
-			stop(message,serverQueue)
-			return
-		}else{
-			message.channel.send("you need to enter a valid command kutte kamine")
+		if(!jitesh(msg,prefix)=="-:-"){
+			msg.reply(jitesh(msg,prefix))
 		}
-}})
-
-const queue=new Map();
-async function execute(message, serverQueue) {
-	const args = message.content.split(" ");
-  
-	const voiceChannel = message.member.voice.channel;
-	if (!voiceChannel)
-	  return message.channel.send(
-		"You need to be in a voice channel to play music!"
-	  );
-	const permissions = voiceChannel.permissionsFor(message.client.user);
-	if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-	  return message.channel.send(
-		"I need the permissions to join and speak in your voice channel!"
-	  );
 	}
-  }
-
+})
 
 client.login(token)
